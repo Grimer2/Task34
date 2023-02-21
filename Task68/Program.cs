ddcbfe04fcc24d3d42f9cebffaf3d6c283e53d2c
Алексей <grimerosha@mail.ru>
2023-02-21 10:18:40 +0500

    Added Task66

---
+++ Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите неотрицательных числа:");
+int number = Convert.ToInt32(Console.ReadLine());
+int stop = Convert.ToInt32(Console.ReadLine());
+
+int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+
+Console.WriteLine(Akk(number, stop)); 
